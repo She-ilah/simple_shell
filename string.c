887c8eabf6a8f53fce11f0dedd84e6cb9a966322
OkumuP <okumupita@gmail.com>
2023-07-28 04:54:23 -0700

    This file contains functions for the string.c for the ALX Simple Shell project

---
+++ string.c
@@ -0,0 +1,57 @@
+#include "shell.h"
+
+/**
+ * get_str_len - Returns the length of a string.
+ * @str: The string whose length is to be determined.
+ *
+ * Return: The length of the string.
+ */
+int get_str_len(char *str)
+{
+	int length = 0;
+
+	if (!str)
+		return (0);
+	while (*str++)
+		length++;
+	return (length);
+}
+/**
+ * my_strcmp - Performs alphabetical comparison of two strings.
+ * @str1: The first string to compare.
+ * @str2: The second string to compare.
+ *
+ * Return: An integer representing the comparison result(-, + or 0)
+ */
+int my_strcmp(char *str1, char *str2)
+{
+	while (*str1 && *str2)
+	{
+		if (*str1 != *str2)
+			return (*str1 - *str2);
+		str1++;
+		str2++;
+	}
+	if (*str1 == *str2)
+		return (0);
+	else
+		return (*str1 < *str2 ? -1 : 1);
+}
+/**
+ * my_strcat - Concatenates two strings.
+ * @destinat: The destination buffer to store the concatenated string.
+ * @source: The source buffer, whose content will be appended.
+ *
+ * Return: Pointer to the destination buffer.
+ */
+char *my_strcat(char *destinat, char *source)
+{
+	char *result = destinat;
+
+	while (*destinat)
+		destinat++;
+	while (*source)
+		*destinat++ = *source++;
+	*destinat = '\0'
+		return (result);
+}
